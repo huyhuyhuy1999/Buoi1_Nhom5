f329427de0595e47a9a028cebdb92a6aa54f5195
Aker <qjayjayq@gmail.com>
2026-03-23 16:15:10 +0700

    Replace clock lab with generated HSI/HSE folders

---
+++ CapXungLock_Nhom-5/HSE/Core/Src/main.c
@@ -0,0 +1,237 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+volatile uint32_t dbg_rcc_cr = 0u;
+volatile uint32_t dbg_rcc_cfgr = 0u;
+volatile uint32_t dbg_rcc_pllcfgr = 0u;
+volatile uint32_t dbg_rcc_ahb1enr = 0u;
+volatile uint32_t dbg_gpioa_moder = 0u;
+volatile uint32_t dbg_gpioa_afrh = 0u;
+volatile uint32_t dbg_sysclk_hz = 0u;
+volatile uint32_t dbg_gpioa_clock_enabled = 0u;
+volatile uint32_t dbg_pa8_mode = 0u;
+volatile uint32_t dbg_pa8_af = 0u;
+volatile uint32_t dbg_hsi_ready = 0u;
+volatile uint32_t dbg_hse_ready = 0u;
+volatile uint32_t dbg_pll_ready = 0u;
+volatile uint32_t dbg_sysclk_sw = 0u;
+volatile uint32_t dbg_sysclk_sws = 0u;
+volatile uint32_t dbg_mco1_source = 0u;
+volatile uint32_t dbg_mco1_prescaler = 0u;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+static void UpdateDebugWatch(void);
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+static void UpdateDebugWatch(void)
+{
+  dbg_rcc_cr = RCC->CR;
+  dbg_rcc_cfgr = RCC->CFGR;
+  dbg_rcc_pllcfgr = RCC->PLLCFGR;
+  dbg_rcc_ahb1enr = RCC->AHB1ENR;
+  dbg_gpioa_moder = GPIOA->MODER;
+  dbg_gpioa_afrh = GPIOA->AFR[1];
+  dbg_sysclk_hz = HAL_RCC_GetSysClockFreq();
+
+  dbg_gpioa_clock_enabled = (RCC->AHB1ENR >> 0) & 0x1u;
+  dbg_pa8_mode = (GPIOA->MODER >> 16) & 0x3u;
+  dbg_pa8_af = (GPIOA->AFR[1] >> 0) & 0xFu;
+  dbg_hsi_ready = (RCC->CR >> 1) & 0x1u;
+  dbg_hse_ready = (RCC->CR >> 17) & 0x1u;
+  dbg_pll_ready = (RCC->CR >> 25) & 0x1u;
+  dbg_sysclk_sw = (RCC->CFGR >> 0) & 0x3u;
+  dbg_sysclk_sws = (RCC->CFGR >> 2) & 0x3u;
+  dbg_mco1_source = (RCC->CFGR >> 21) & 0x3u;
+  dbg_mco1_prescaler = (RCC->CFGR >> 24) & 0x7u;
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  UpdateDebugWatch();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    UpdateDebugWatch();
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
